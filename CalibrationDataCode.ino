c565f6440859be43bf40236b1c8e22448299b648
Elec Dash Tron <leon@elec-tron.org>
2020-01-09 12:46:01 +1100

    Add files via upload

--- CalibrationDataCode.ino
+++ CalibrationDataCode.ino
@@ -13,7 +13,7 @@ Intented to output
 const int MPU=0x68; 
 int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
 long accellData[2][3], gyroData[2][3];
-float rangedAccellData[3], rangedGyroData[3];
+float rangedAccellData[3], rangedGyroData[3], rangedAngularData[3], prevRangedGyroData[3], prevRangedAccellData[3];
 int accellFlutter[3] = {0, 0, 0};
 int gyroFlutter[3] = {0, 0, 0};
 int accellPeakValues[3] = {32768, 32768, 32768};
@@ -22,9 +22,9 @@ float accellFilters[3] = {0,0,0};
 float gyroFilters[3] = {0,0,0};
 int ticker=1, aCnt=0;
 int calibrationRuns = 500, calibrationDelay = 5, caibrationMultiplier=2, sampleRate = 10;
-
+short int maxRotationMultiplier[3] = {22, 22, 22};
 //I2C Data & Clock pin foniguration, choose as you like
-const byte dataPIN = D3, clockPIN = D4;
+const byte dataPIN = D1, clockPIN = D2;
 
 
 void setup()
@@ -41,7 +41,7 @@ void setup()
   Wire.write(0);    
   Wire.endTransmission(true);
   
-  Serial.printf("\r\nStart Calibration routine for %d cycles...\r\n", calibrationRuns);
+  Serial.printf("\r\nStart Acc/Gyro Calibration routine for standing noise %d cycles...\r\n", calibrationRuns);
   for(ticker=0; ticker<calibrationRuns; ticker++)
   {
     sampleIMU();
@@ -57,15 +57,14 @@ void setup()
       Serial.printf("%d\t", ticker);
     }
   }
-  Serial.printf("\r\nCalibration complete");
   for(aCnt=0; aCnt<3; aCnt++)
   {
     accellData[1][aCnt] = accellData[0][aCnt]/calibrationRuns;
     gyroData[1][aCnt] = gyroData[0][aCnt]/calibrationRuns;
   }
-  Serial.printf("\r\nAX_Flutt\tAY_Flutt\tAZ_Flutt\t|\tGX_Flutt\tGY_Flutt\tGZ_Flutt");
-  Serial.printf("\r\n%d\t%d\t%d\t\t\t|\t\t\t%d\t%d\t%d", accellData[1][0], accellData[1][1], accellData[1][2], gyroData[1][0], gyroData[1][1], gyroData[1][2]);
-  
+  Serial.printf("\r\nStanding Noise Calibration complete.");
+  Serial.printf("\r\n\tAX_Flutt\tAY_Flutt\tAZ_Flutt\t|\tGX_Flutt\tGY_Flutt\tGZ_Flutt");
+  Serial.printf("\r\n\t%d\t%d\t%d\t\t\t|\t\t%d\t%d\t%d", accellData[1][0], accellData[1][1], accellData[1][2], gyroData[1][0], gyroData[1][1], gyroData[1][2]);
   //Set Accel avg flutter rates
   //Set GYRO avg flutter rates
   for(aCnt=0; aCnt<3; aCnt++)
@@ -77,12 +76,12 @@ void setup()
   //Set Gyro peak range
   for(aCnt=0; aCnt<3; aCnt++)
   {
-    accellPeakValues[aCnt]-=abs(accellFlutter[aCnt]);
-    gyroPeakValues[aCnt]-=abs(gyroFlutter[aCnt]);
+    accellPeakValues[aCnt] -= abs(accellFlutter[aCnt]);
+    gyroPeakValues[aCnt] -= abs(gyroFlutter[aCnt]);
+    gyroPeakValues[aCnt] = sqrt(gyroPeakValues[aCnt]);
   }
   delay(1000);
-  
-  Serial.printf("\r\nStart calibration of filters for %d cycles...\r\n", calibrationRuns);
+  Serial.printf("\r\nStart calibration of input filters for %d cycles...\r\n", calibrationRuns);
   for(ticker=0; ticker<calibrationRuns; ticker++)
   {
     sampleIMU();
@@ -95,14 +94,14 @@ void setup()
 
     for(aCnt=0; aCnt<3; aCnt++)
     {
-      accellFilters[aCnt] += (((float)accellData[0][aCnt])/accellPeakValues[aCnt])*100;
+      accellFilters[aCnt] += ((float)accellData[0][aCnt])/accellPeakValues[aCnt];
       if(gyroData[0][aCnt]>0)
       {
-        gyroFilters[aCnt] += ( sqrt( gyroData[0][aCnt] ) / sqrt( gyroPeakValues[aCnt] ) ) * 100;
+        gyroFilters[aCnt] +=  sqrt( gyroData[0][aCnt] ) / gyroPeakValues[aCnt] ;
       }
       else
       {
-        gyroFilters[aCnt] += (( sqrt( abs(gyroData[0][aCnt]) ) / sqrt( gyroPeakValues[aCnt] ) ) * 100);
+        gyroFilters[aCnt] +=  sqrt( abs(gyroData[0][aCnt]) ) / gyroPeakValues[aCnt] ;
       }
     }
     delay(calibrationDelay);
@@ -120,50 +119,15 @@ void setup()
   Serial.printf("\r\nFilter Calibration complete....");
   Serial.printf("\r\nAx\t%f\tAy\t%f\tAz\t%f\tGx\t%f\tGy\t%f\tGz\t%f", accellFilters[0],accellFilters[1],accellFilters[2],gyroFilters[0],gyroFilters[1],gyroFilters[2]);
   Serial.printf("\r\nIMU CALIBRATION COMPLETE\r\n");
-  delay(1000);
+  delay(2000);
 }
 
 void loop()
 {
-  //Take sampleRate samples
-  for(aCnt=0; aCnt<sampleRate; aCnt++)
-  {
-    sampleIMU();
-    //Read in raw values using the flutter rate to clip down to 0
-    accellData[0][0] += AcX-accellFlutter[0];
-    accellData[0][1] += AcY-accellFlutter[1];
-    accellData[0][2] += AcZ-accellFlutter[2];
-    gyroData[0][0] += GyX-gyroFlutter[0];
-    gyroData[0][1] += GyY-gyroFlutter[1];
-    gyroData[0][2] += GyZ-gyroFlutter[2];
-  }
-  for(aCnt=0; aCnt<3; aCnt++)
-  {
-    accellData[0][aCnt] = accellData[0][aCnt]/sampleRate;
-    gyroData[0][aCnt] = gyroData[0][aCnt]/sampleRate;
-  }
-  
-  //calculate ratio based on peak
-  for(aCnt=0; aCnt<3; aCnt++)
-  {
-    rangedAccellData[aCnt] = (((float)accellData[0][aCnt])/accellPeakValues[aCnt])*100;
-    if(gyroData[0][aCnt]>0)
-    {
-      rangedGyroData[aCnt] = ( sqrt( gyroData[0][aCnt] ) / sqrt( gyroPeakValues[aCnt] ) ) * 100;
-    }
-    else
-    {
-      rangedGyroData[aCnt] = -(( sqrt( abs(gyroData[0][aCnt]) ) / sqrt( gyroPeakValues[aCnt] ) ) * 100);
-    }
-    
-    if( rangedAccellData[aCnt]<accellFilters[aCnt] && rangedAccellData[aCnt]>-accellFilters[aCnt] ){rangedAccellData[aCnt]=0;}
-    if( rangedGyroData[aCnt]<gyroFilters[aCnt] && rangedGyroData[aCnt]>-gyroFilters[aCnt] ){rangedGyroData[aCnt]=0;}
-  }
-  //Serial.printf("\r\n%f\t%f\t%f\t%f\t%f\t%f", rangedAccellData[0], rangedAccellData[1], rangedAccellData[2], rangedGyroData[0], rangedGyroData[1], rangedGyroData[2]);
-  Serial.printf("\r\n%f\t%f\t%f\t%f\t%f\t%f", round(rangedAccellData[0]), round(rangedAccellData[1]), round(rangedAccellData[2]), round(rangedGyroData[0]), round(rangedGyroData[1]), round(rangedGyroData[2]));
-  //Serial.printf("\r\n%f\t%f\t%f", round(rangedGyroData[0]), round(rangedGyroData[1]), round(rangedGyroData[2]));
-  //Serial.printf("\r\n%f", round(rangedGyroData[0]));
-  delay(10);
+  calcIMUData();
+  trackAngle();
+  Serial.printf("\r\n%f\t%f\t%d", rangedGyroData[0]*100, rangedAngularData[0], ((int)rangedAngularData[0])%360 );
+  delay(20);
 }
 
 void sampleIMU()
@@ -182,4 +146,80 @@ void sampleIMU()
   GyX=Wire.read()<<8|Wire.read();  
   GyY=Wire.read()<<8|Wire.read();  
   GyZ=Wire.read()<<8|Wire.read();  
+}
+
+void calcIMUData()
+{
+  //Reset Accell/Gyro Counters
+  for(aCnt=0; aCnt<3; aCnt++)
+  {
+    //Store previous readings to analyise gradient slope direction
+    prevRangedAccellData[aCnt] = rangedAccellData[aCnt];
+    prevRangedGyroData[aCnt] = rangedGyroData[aCnt];
+    //clear out values for this read
+    accellData[0][aCnt] = 0;
+    gyroData[0][aCnt]=0;
+  }
+  //Take sampleRate number of samples for averaging  
+  for(aCnt=0; aCnt<sampleRate; aCnt++)
+  {
+    sampleIMU();
+    //Read in raw values
+    accellData[0][0] += AcX-accellFlutter[0];
+    accellData[0][1] += AcY-accellFlutter[1];
+    accellData[0][2] += AcZ-accellFlutter[2];
+    gyroData[0][0] += GyX-gyroFlutter[0];
+    gyroData[0][1] += GyY-gyroFlutter[1];
+    gyroData[0][2] += GyZ-gyroFlutter[2];
+  }
+  for(aCnt=0; aCnt<3; aCnt++)
+  {
+    accellData[0][aCnt] = accellData[0][aCnt]/sampleRate;
+    gyroData[0][aCnt] = gyroData[0][aCnt]/sampleRate;
+  }
+  
+  //calculate ratio based on peak
+  for(aCnt=0; aCnt<3; aCnt++)
+  {
+    rangedAccellData[aCnt] = ((float)accellData[0][aCnt]) / accellPeakValues[aCnt];
+    if(gyroData[0][aCnt]>0)
+    {
+      rangedGyroData[aCnt] = sqrt( gyroData[0][aCnt] ) / gyroPeakValues[aCnt];
+    }
+    else
+    {
+      rangedGyroData[aCnt] = -( sqrt( abs(gyroData[0][aCnt]) ) / gyroPeakValues[aCnt] );
+    }
+    
+    if( rangedAccellData[aCnt]<accellFilters[aCnt] && rangedAccellData[aCnt]>-accellFilters[aCnt] ){rangedAccellData[aCnt]=0;}
+    if( rangedGyroData[aCnt]<gyroFilters[aCnt] && rangedGyroData[aCnt]>-gyroFilters[aCnt] ){rangedGyroData[aCnt]=0;}
+  }
+}
+
+float getSlope(float x1, float x2)
+{
+  float slope=0;
+  
+  if(x1!=x2)
+  {
+    slope = 1 / (x2-x1);
+  }
+  return slope;
+}
+
+void trackAngle()
+{
+  float currentSlope = 0;
+  for(ticker=0; ticker<3; ticker++)
+  {
+    currentSlope = getSlope(prevRangedGyroData[ticker], rangedGyroData[ticker]);
+    if(currentSlope>0)
+    {
+        rangedAngularData[ticker] += (maxRotationMultiplier[ticker]*rangedGyroData[ticker]);
+    }
+    else if(currentSlope<0)
+    {
+        rangedAngularData[ticker] -= maxRotationMultiplier[ticker] * abs(rangedGyroData[ticker]);
+    }
+  }
 }
